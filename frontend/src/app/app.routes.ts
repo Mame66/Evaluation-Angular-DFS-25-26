1944fa078e89e0b0f0e466a09871598cd869bb8b
Mame <mamegorciss5@gmail.com>
2026-02-24 16:25:40 +0100

    ✨ feat: code session login and game

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,10 +1,11 @@
 import { Routes } from '@angular/router';
-//import { LoginComponent } from './components/login/login.component';
-//import { SessionListComponent } from './components/session-list/session-list.component';
-//import { GameComponent } from './components/game/game.component';
+import { LoginComponent } from './components/login/login';
+import { SessionListComponent } from './components/session-list/session-list';
+import { GameComponent } from './components/game/game';
 
 export const routes: Routes = [
-  //{ path: '', component: LoginComponent },
-  //{ path: 'sessions', component: SessionListComponent },
-  //{ path: 'game/:id', component: GameComponent }
+  { path: '', component: LoginComponent },
+  { path: 'sessions', component: SessionListComponent },
+  { path: 'game/:id', component: GameComponent },
+  { path: '**', redirectTo: '' }
 ];
