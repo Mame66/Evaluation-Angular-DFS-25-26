ae943c0a2fb5d81929ff34bd387687770f6d0699
Mame <mamegorciss5@gmail.com>
2026-02-24 12:17:03 +0100

    Ignore node_modules

---
+++ backend/data/utilisateurs.js
@@ -0,0 +1,16 @@
+const utilisateurs = [
+    {
+        email: "a@a.com",
+        password: "root",
+        admin: true },
+    {
+        email: "b@b.com",
+        password: "azerty",
+        admin: false },
+    {
+        email: "c@c.com",
+        password: "qwerty",
+        admin: false }
+];
+
+module.exports = utilisateurs;
